dcf120972ba07e80886ea01829b11a4910131798
Arjun Manjunath <42509444+arjun120@users.noreply.github.com>
2020-04-03 03:21:00 -0500

    initial commit

---
+++ app/src/main/java/app1/example/payse/nav_header.java
@@ -0,0 +1,17 @@
+package app1.example.payse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class nav_header extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_header);
+
+    }
+}
---
+++ app/src/main/java/app1/example/payse/payment_options.java
@@ -0,0 +1,64 @@
+package app1.example.payse;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import static app1.example.payse.R.*;
+
+public class payment_options extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private TextView pname;
+    String fullname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_payment_options);
+        toolbar = findViewById(id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(id.drawer_layout);
+        NavigationView navigationView = findViewById(id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer ,toolbar, R.string.navigation_draw_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case id.nav_transactions:
+                startActivity(new Intent(payment_options.this,recent_payments.class));
+                break;
+        }
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if( drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+
+    }
+
+
+}
